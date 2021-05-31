086b2f52c8fe46ab09527cde2474ecc71eea48c4
EdacheAndrew <60603459+EdacheAndrew@users.noreply.github.com>
2021-05-31 11:11:37 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://admin:ekle.3353@cluster0.achkq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://admin:yourpassword@cluster0.achkq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
